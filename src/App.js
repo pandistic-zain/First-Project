6b51605a5cbc201028719ca495bd74a6080b4cfe
Zain <zainsaith7@gmail.com>
2023-08-31 16:53:21 +0500

    4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/Texts";
 function App() {
   return (
-<>
-
-</>
+    <>
+      <Navbar title="Fitness Zone" />
+      
+      <div className = "container my-3">
+      <TextForm heading="Enter Text to Analyse" />
+      </div>
+      
+    </>
   );
 }
 
---
+++ src/Components/Texts.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function TextForm(props) {
+  return (
+    <div>
+      <div className="mb-3">
+        <h1>{props.heading}</h1>
+
+        <label for="myBox" className="form-label">
+           TextArea
+        </label>
+        <textarea
+          className="form-control"
+          id="myBox"
+          rows="8"
+        ></textarea>
+        
+        <button className="btn Primary">Convert to UpperCase</button>
+      </div>
+    </div>
+  );
+}
+Texts.PropTypes =
+{
+ headings : PropTypes.string.isRequired
+}
