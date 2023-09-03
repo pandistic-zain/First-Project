f38d48eaedbc265e001dc8a2e0e9aae7c819d110
Zain <zainsaith7@gmail.com>
2023-09-03 15:42:55 +0500

    3

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function About() {
   return (
     <div className="container">
-        <h2>
+        <h1>
             About
-        </h2>
+        </h1>
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
           <h2 className="accordion-header">
