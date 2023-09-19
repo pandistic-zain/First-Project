1e5cb7012dd1af3256e0616db15d3cd1f0e83a3e
Zain <zainsaith7@gmail.com>
2023-09-18 21:14:22 -0700

    19-Sep 2nd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import React, { useState } from 'react'
 
 function App() {
   const[mode, setMode] = useState('light');
-  const[alert,setAlert] = useState(null)
+  const[alert,setAlert] = useState('null');
   const showAlert=(message,type)=>{
     setAlert(
       {
@@ -22,19 +22,19 @@ function App() {
     {
     setMode( 'light'); 
     document.body.style.backgroundColor='white';
-    showAlert("The Light Mode Has been Enabled","success");
+    showAlert("The Light Mode Has been Enabled", "success");
     }
     else
     {
     setMode( 'dark'); 
     document.body.style.backgroundColor='#0B1A4F';
-    showAlert("The Dark Mode Has been Enabled","success");
+    showAlert("The Dark Mode Has been Enabled", "success");
     }
 }
   return (
     <>
       <Navbar title="Fitness Zone" mode={mode} ToggleMode={ToggleMode} />
-      {/* <Alert alert= "This is Alert"/> */}
+      <Alert/>
       <div className = "container my-3"> 
       <TextForm heading="Enter Text to Analyse" mode={mode}/>
      <About mode={mode} ToggleMode={ToggleMode}/>
